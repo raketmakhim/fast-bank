d2b0dd9a928feb700c0863cf0ba2e013d8ccc929
rmakhim <raketmakhim@gmail.com>
2025-09-24 19:29:27 +0100

    initial commit

---
+++ src/test/java/com/fastbank/fast_bank/FastBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastbank.fast_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
